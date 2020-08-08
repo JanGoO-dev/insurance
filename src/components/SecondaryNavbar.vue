21e2976a50098a8a160e1bdd96f2c48f2a23875c
JanGoO-coder <asadalipkp9@gmail.com>
2020-08-08 15:26:03 +0500

    change the back color of mini navigation

--- src/components/SecondaryNavbar.vue
+++ src/components/SecondaryNavbar.vue
@@ -3,11 +3,8 @@
     <header id="mini-nav-wrap" class="py-2">
       <section id="mini-nav-links">
         <ul class="nav">
-          <li v-for="(link, index) in 6" :key="index" class="mx-2">
-            <button
-              type="submit"
-              class="btn btn-outline-secondary btn-sm px-3 round-left round-right"
-            >
+          <li v-for="(link, index) in 6" :key="index" class="mx-1">
+            <button type="submit" class="btn btn-outline-info btn-sm px-5">
               Link {{ index }}
             </button>
           </li>
@@ -22,30 +19,38 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.round-left {
-  -webkit-border-top-left-radius: 354px;
-  -webkit-border-bottom-left-radius: 354px;
-  -moz-border-radius-topleft: 354px;
-  -moz-border-radius-bottomleft: 354px;
-  border-top-left-radius: 354px;
-  border-bottom-left-radius: 354px;
+.nav li:first-child {
+  button {
+    -webkit-border-top-left-radius: 354px;
+    -webkit-border-bottom-left-radius: 354px;
+    -moz-border-radius-topleft: 354px;
+    -moz-border-radius-bottomleft: 354px;
+    border-top-left-radius: 354px;
+    border-bottom-left-radius: 354px;
+  }
 }
-
-.round-right {
-  -webkit-border-top-right-radius: 354px;
-  -webkit-border-bottom-right-radius: 354px;
-  -moz-border-radius-topright: 354px;
-  -moz-border-radius-bottomright: 354px;
-  border-top-right-radius: 354px;
-  border-bottom-right-radius: 354px;
+.nav li:last-child {
+  button {
+    -webkit-border-top-right-radius: 354px;
+    -webkit-border-bottom-right-radius: 354px;
+    -moz-border-radius-topright: 354px;
+    -moz-border-radius-bottomright: 354px;
+    border-top-right-radius: 354px;
+    border-bottom-right-radius: 354px;
+  }
 }
 #mini-nav-wrap {
-  background-color: #ececec;
+  // background-color: #ececec;
+  // background-color: rgba(95, 255, 3, 0.151);
+  // background-color: rgba(255, 3, 3, 0.075);
   display: grid;
   place-items: center;
-  -webkit-box-shadow: inset 0px 5px 29px -9px rgba(0, 0, 0, 0.25);
-  -moz-box-shadow: inset 0px 5px 29px -9px rgba(0, 0, 0, 0.25);
-  box-shadow: inset 0px 5px 29px -9px rgba(0, 0, 0, 0.25);
+  // -webkit-box-shadow: inset 0px 0px 29px -9px #0ec6cc60;
+  // -moz-box-shadow: inset 0px 0px 29px -9px #0ec6cc60;
+  // box-shadow: inset 0px 0px 29px -9px #0ec6cc60;
+  -webkit-box-shadow: inset 0px 0px 69px -39px #0059ff60;
+  -moz-box-shadow: inset 0px 0px 69px -39px #0059ff60;
+  box-shadow: inset 0px 0px 69px -39px #0059ff60;
 }
 
 button[type="submit"] {
